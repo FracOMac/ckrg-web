028b59dc17d865f74c84384571fd6bc189e8fc0f
Ryan J Seaman <ryan@seaman.email>
2023-11-17 03:34:07 -0700

    made links relative to parent directory so they'd work on github pages

--- src/Components/SiteHeader/NavBar.tsx
+++ src/Components/SiteHeader/NavBar.tsx
@@ -3,8 +3,8 @@ import NavElement from "./NavElement";
 function NavBar() {
     return (
         <div className="navBar">
-            <NavElement name="Home" link="/"/>
-            <NavElement name="Calc" link="/calc"/>
+            <NavElement name="Home" link="../"/>
+            <NavElement name="Calc" link="../calc"/>
         </div>
     )
 }
